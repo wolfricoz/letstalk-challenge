899a6d9c8d6e674b0d5c90dcb4ac57f8b5f306c1
wolfricoz <ricoisdenaam@gmail.com>
2024-07-08 18:11:18 +0200

    07:02 Forgot to upload this file; it adds the currency update command to the schedule

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+Artisan::command('currency:update', function () {
+    $this->comment('Updating currencies');
+})->purpose('Update currencies')->at('01:00')->at('13:00');
