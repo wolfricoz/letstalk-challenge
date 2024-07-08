b593d9955e5c5be16111fad848297987c10a7d34
wolfricoz <ricoisdenaam@gmail.com>
2024-07-08 12:54:59 +0200

    02:31:00: Added tests and resolved the login issue. The issue was that laravel hashes and I was trying to use encryption.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,7 +53,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Crypt::encryptString($request->password),
+            'password' => Hash::make($request->password),
         ]);
 
         auth()->login($user);
---
+++ tests/Feature/AuthControllerTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class AuthControllerTest extends TestCase
+{
+//  These tests were written with help from Copilot.
+    use RefreshDatabase, WithoutMiddleware;
+
+    public function testIndex()
+    {
+        $response = $this->get(route('login'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.login');
+    }
+
+    public function testRegister()
+    {
+        $response = $this->get(route('register'));
+
+        $response->assertStatus(200);
+        $response->assertViewIs('auth.register');
+    }
+
+    public function testLogout()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->post(route('logout'));
+
+        $response->assertRedirect(route('home'));
+        $this->assertGuest();
+    }
+
+    public function testAuthenticate()
+    {
+        $user = User::factory()->create([
+            'password' => Hash::make('Password123456'),
+        ]);
+
+        $response = $this->post(route('login.authenticate'), [
+            'email' => $user->email,
+            'password' => 'Password123456',
+        ]);
+
+        $response->assertRedirect(route('home'));
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function testStore()
+    {
+        $userData = [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+            'password' => 'Password123456',
+            'password_confirmation' => 'Password123456',
+        ];
+
+        $response = $this->put(route('register.store'), $userData);
+
+        $response->assertRedirect(route('home'));
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'Test User',
+            'email' => 'testuser@example.com',
+        ]);
+
+        $user = User::where('email', 'testuser@example.com')->first();
+        echo crypt::encrypt('Password123456') . PHP_EOL;
+
+        $this->assertTrue(Hash::check('Password123456', $user->password));
+        $this->assertAuthenticatedAs($user);
+    }
+}
