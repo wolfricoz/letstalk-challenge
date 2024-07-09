987c78961f555fd139c9e41852c8fc75c30bdb20
wolfricoz <ricoisdenaam@gmail.com>
2024-07-09 11:55:31 +0200

    08:50 Removing some old code from console

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,4 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
-Schedule::command('currency:update')->purpose('Update currencies')->at('01:00')->at('13:00');
+Schedule::command('currency:update')->at('01:00')->at('13:00');
