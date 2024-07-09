3711e1bf61abb8f73340deab3b0589548cf515d1
wolfricoz <ricoisdenaam@gmail.com>
2024-07-09 11:53:39 +0200

    08:39 Bug Fix: Console overwrote the update command, fixed by actually properly scheduling.

--- routes/console.php
+++ routes/console.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-Artisan::command('currency:update', function () {
-    $this->comment('Updating currencies');
-})->purpose('Update currencies')->at('01:00')->at('13:00');
+Schedule::command('currency:update')->purpose('Update currencies')->at('01:00')->at('13:00');
